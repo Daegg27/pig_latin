094806c220e4ff31a0abf6975b863e1be4e7ae65
Dalton Eggleston <egglestondalton@yahoo.com>
2022-06-06 13:24:38 -0700

    halfway, working on budget

--- js/pigLatin.js
+++ js/pigLatin.js
@@ -1,2 +1,22 @@
 exports.translate = function(word) {
+
+    splitWord = word.split(" ")
+    finalWord = [];
+    vowelArray = ["A","E","I","O","U"]
+    
+
+    splitWord.forEach(element => {
+        if (element.charAt(0).match(/[aeiou]/) || element.charAt(0).match(/[AEIOU]/)){
+            console.log("correct")
+            element += "ay"
+            finalWord.push(element)
+        }else{
+            finalWord.push(element)
+        }
+        console.log(typeof element)
+    });
+    console.log(finalWord)
+    
+
 };
+exports.translate("apple pie")

0a2db23df76be8e4f75c1b60b142898d18b96356
Dalton Eggleston <egglestondalton@yahoo.com>
2022-06-06 17:55:06 -0700

    partially done

--- js/pigLatin.js
+++ js/pigLatin.js
@@ -2,21 +2,45 @@ exports.translate = function(word) {
 
     splitWord = word.split(" ")
     finalWord = [];
-    vowelArray = ["A","E","I","O","U"]
     
-
-    splitWord.forEach(element => {
-        if (element.charAt(0).match(/[aeiou]/) || element.charAt(0).match(/[AEIOU]/)){
-            console.log("correct")
-            element += "ay"
-            finalWord.push(element)
+    for (let i = 0; i < splitWord.length; i++){
+        if (splitWord[i].charAt(0).match(/[aeiou]/) || splitWord[i].charAt(0).match(/[AEIOU]/)){
+            splitWord[i] += "ay"
         }else{
-            finalWord.push(element)
+            rearrangedWord = [];
+            vowelDetected = false;
+            for (let c = 0; c < splitWord[i][c].length; c++){
+                if (splitWord[i].charAt(c).match(/[^aeiou]/) || splitWord[i].charAt(c).match(/[^AEIOU]/) && vowelDetected == false){
+                    rearrangedWord.push(splitWord[i][c])
+                }else{
+                    rearrangedWord.splice(0, 0, splitWord[i][c])
+                    vowelDetected = true
+                }
+            }
+            rearrangedWord.push("ay")
+            console.log(rearrangedWord)
+            createdWord = rearrangedWord.join("")
         }
-        console.log(typeof element)
-    });
-    console.log(finalWord)
+    }
+    console.log(splitWord)
+    console.log(createdWord)
+  
+  
+  
+  
+  
+    // splitWord.forEach(element => {
+    //     if (element.charAt(0).match(/[aeiou]/) || element.charAt(0).match(/[AEIOU]/)){
+    //         console.log("correct")
+    //         element += "ay"
+    //         finalWord.push(element)
+    //     }else{
+    //         finalWord.push(element)
+    //     }
+    //     console.log(typeof element)
+    // });
+    // console.log(finalWord)
     
 
 };
-exports.translate("apple pie")
+exports.translate("apple school")
